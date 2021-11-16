5977a94c6f2a517e489b37ec2b8df440b2fcf78f
Pockyyyyyy <Pockyyyyyy@users.noreply.github.com>
2021-11-16 17:00:39 -0500

    Running animal facts

---
+++ Lab 6A/script.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(400, 400);
+  getAnimalFact().then((data) => {
+    console.log(data);
+ })
+}
+
+function draw() {
+  background(220);
+}
+
+function getAnimalFact(){
+  var fact = Math.floor(Math.random() * 5) + 1
+  var URL = 'https://anime-facts-rest-api.herokuapp.com/api/v1/fma_brotherhood/' + fact.toString()
+  return fetch(URL, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    // console.log(data.data.fact)
+    return data.data.fact
+  }).catch(err => {
+    console.log(err)
+  })
+}
