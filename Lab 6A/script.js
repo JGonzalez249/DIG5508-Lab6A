e671b8614b601df8806df872b63076625f6be4fc
jgonzalez249 <46466482+JGonzalez249@users.noreply.github.com>
2021-11-17 00:27:47 -0500

    running out of ideas

--- Lab 6A/script.js
+++ Lab 6A/script.js
@@ -1,17 +1,18 @@
 function setup() {
   createCanvas(400, 400);
-  getAnimalFact().then((data) => {
+  getAnimeFact().then((data) => {
     console.log(data);
  })
 }
 
 function draw() {
   background(220);
+
 }
 
-function getAnimalFact(){
+function getAnimeFact(){
   var fact = Math.floor(Math.random() * 5) + 1
-  var URL = 'https://anime-facts-rest-api.herokuapp.com/api/v1/' + fact.toString()
+  var URL = 'https://anime-facts-rest-api.herokuapp.com/api/v1/jujutsu_kaisen/' + fact.toString()
   return fetch(URL, {
     method: 'GET',
     headers: {
@@ -20,7 +21,7 @@ function getAnimalFact(){
   }).then(response => {
     return response.json();
   }).then(data => {
-     console.log(data.data.fact)
+     //console.log(data.data.fact)
     return data.data.fact
   }).catch(err => {
     console.log(err)
